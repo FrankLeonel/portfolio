191f885a924a8a99dbaa0182890639ba21d90665
FrankLeonel <frank.leonel@alu.ufc>
2022-05-25 16:28:09 -0300

    criação do header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import Content from "components/Content";
+import * as S from "./Header.style";
+
+const Header = () => {
+  return (
+    <Content>
+      <S.HeaderContainer>Put your code here</S.HeaderContainer>
+    </Content>
+  );
+};
+
+export default Header;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,10 @@
 import Footer from "components/Footer";
+import Header from "components/Header";
 
 const Home = () => {
   return (
     <>
+      <Header />
       <h1>Home</h1>
 
       <Footer />
