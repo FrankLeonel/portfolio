83bc57b4fa2196baf4e0d8032da120ca3e76ad68
FrankLeonel <frank.leonel@alu.ufc>
2022-06-07 10:52:05 -0300

    criação do componente

---
+++ src/components/Skills/Skills.js
@@ -0,0 +1,12 @@
+import Content from "components/Content";
+import * as S from "./Skills.style";
+
+const Skills = () => {
+  return (
+    <S.SkillsContainer id="skills">
+      <Content>Put your code here</Content>
+    </S.SkillsContainer>
+  );
+};
+
+export default Skills;
